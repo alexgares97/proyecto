e5fe2b050a98afca8d1cb425f70449cc1139f875
alexgares97 <44832264+alexgares97@users.noreply.github.com>
2023-06-09 17:27:58 +0200

    Delete src directory

--- src/www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion = new PDO('mysql:host=mysql;dbname=project', 'user', '1234');
-// Utilizar la conexión aquí
-$resultados = $conexion->query("SELECT * FROM usuarios");
-foreach($resultados as $fila){
-echo $fila['id'] . " - " .$fila['nombre'] . '</br>';
-}
-echo "Hola mundo";
