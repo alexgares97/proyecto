469707f2fe5ab5b2899a98f1910137bb1e6d13c3
alexgares97 <44832264+alexgares97@users.noreply.github.com>
2023-06-12 17:49:04 +0200

    Update Element.php

--- my-project/app/Models/Element.php
+++ my-project/app/Models/Element.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Element extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id', 'title', 'author', 'date', 'desc', 'link', 'image', 'tags'];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
 }
+
